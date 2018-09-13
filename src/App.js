5e355ecd441077d9b0f750807ed7a88505a25e8a
wrmtpp <woramet2539@icloud.com>
2018-09-13 10:51:59 +0700

    check point 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
+import CharacterCard from './CharacterCard';
 class App extends Component {
- render() {
- return (
- <div className="App">
- Hello World
- </div>
- );
- }
+  render() {
+    return (
+      <div className="App">
+          <CharacterCard value="h"/>
+          <CharacterCard value="i"/>
+      </div>
+    );
+  }
 }
+
 export default App;
