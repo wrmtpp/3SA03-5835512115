aa401b59ff3b12ad177b221e8248fc76852971ca
wrmtpp <woramet2539@icloud.com>
2018-09-13 11:11:47 +0700

    check point 2.1

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CharacterCard from './CharacterCard';
+const word = "Hello";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <CharacterCard value="h"/>
-          <CharacterCard value="i"/>
-      </div>
+<div>
+    {
+      Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+    }
+ </div>
     );
   }
 }
